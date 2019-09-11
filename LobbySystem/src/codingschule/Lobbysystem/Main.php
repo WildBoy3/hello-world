dc0b3f105e11c7e517f3b04d6f7f2fbc1316e686
WildBoy3 <55068771+WildBoy3@users.noreply.github.com>
2019-09-12 00:11:26 +0200

    Update Main.php

--- LobbySystem/src/codingschule/Lobbysystem/Main.php
+++ LobbySystem/src/codingschule/Lobbysystem/Main.php
@@ -5,6 +5,7 @@ namespace test;
 use pocketmine\event\Listener;
 use pocketmine\Player;
 use pocketmine\utils\TextFormat;
+use codingschule\Lobbysystem\Teleport;
 
 class Main extends PluginBase{
     public function onEnable()
