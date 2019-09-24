66b9818506c738be3de93682f8595a9b8a413b8f
WildBoy3 <55068771+WildBoy3@users.noreply.github.com>
2019-09-24 23:42:04 +0200

    Update Main.php

--- LobbySystem/src/codingschule/Lobbysystem/Main.php
+++ LobbySystem/src/codingschule/Lobbysystem/Main.php
@@ -11,4 +11,11 @@ class Main extends PluginBase{
     public function onEnable()
     {
     }
+    public function Drop(PlayerDropItemEvent $event){
+        $player = $event->getPlayer();
+        if ($player->getGamemode() == 0 or $player->getGamemode() == 2){
+            $event->setCancelled(true);
+        }
+
+    }
 }
